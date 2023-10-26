9dae552d5e160caad57e8e0ea411f3016babb823
稻草人 <125216585+ouyangzq@users.noreply.github.com>
2023-10-27 05:06:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,16 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-#echo src-git helloworld https://github.com/fw876/helloworld.git >>feeds.conf.default
-#echo src-git oui https://github.com/zhaojh329/oui.git >>feeds.conf.default
-#echo src-git video https://github.com/openwrt/video.git >>feeds.conf.default
-#echo src-git targets https://github.com/openwrt/targets.git >>feeds.conf.default
-#echo src-git oldpackages http://git.openwrt.org/packages.git >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.defaultt
 #echo src-link custom /usr/src/openwrt/custom-feed >>feeds.conf.default
